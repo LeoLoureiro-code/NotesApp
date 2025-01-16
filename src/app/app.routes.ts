31732d8c52e2f8864f7a747a0c889ea589fba570
Leo Loureiro <leolou.90@gmail.com>
2025-01-15 18:23:50 -0700

    add routes to the website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from '../pages/login-page/login-page.component';
+import { NotesPageComponent } from '../pages/notes-page/notes-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: 'login', component: LoginPageComponent},
+{path: 'notes', component: NotesPageComponent},
+];
