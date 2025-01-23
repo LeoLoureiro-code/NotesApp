0db9f94787be17ecdd0f68f345fd2cdcc535d139
Leo Loureiro <leolou.90@gmail.com>
2025-01-22 18:53:38 -0700

    Add default path to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { RegisterComponent } from '../components/register/register.component';
 import { LoginComponent } from '../components/login/login.component';
 
 export const routes: Routes = [
-{path: '', redirectTo: 'home', pathMatch: 'full'},    
+{path: '', redirectTo: 'login', pathMatch: 'full'},    
 {path: 'login', component: LoginComponent},
 {path: 'register', component: RegisterComponent},
 ];
