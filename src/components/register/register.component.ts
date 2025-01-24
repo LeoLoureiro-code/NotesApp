2688a6a004061af2c64b720502b94b29a9507afe
Leo Loureiro <leolou.90@gmail.com>
2025-01-23 19:00:32 -0700

    add routerlinks and finish styles

--- src/components/register/register.component.ts
+++ src/components/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'register',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
