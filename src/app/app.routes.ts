3ebcd90fa68e9058a068f5742a48670c6135d196
Leo Loureiro <leolou.90@gmail.com>
2025-01-23 19:07:15 -0700

    add path to forgot password component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from '../components/register/register.component';
 import { LoginComponent } from '../components/login/login.component';
+import { ForgotPasswordComponent } from '../components/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
 {path: '', redirectTo: 'login', pathMatch: 'full'},    
 {path: 'login', component: LoginComponent},
 {path: 'register', component: RegisterComponent},
+{path: 'forgot-password', component: ForgotPasswordComponent},
 ];
