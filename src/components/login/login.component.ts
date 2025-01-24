03010fbe8d299667a1bbbfbb7814104316ad54d7
Leo Loureiro <leolou.90@gmail.com>
2025-01-23 18:59:54 -0700

    add routerlink

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
