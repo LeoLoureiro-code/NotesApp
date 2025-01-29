5136f9162e38385fe58928467df10a5c0bd7c753
Leo Loureiro <leolou.90@gmail.com>
2025-01-28 19:23:15 -0700

    add all components for notes

---
+++ src/components/all-notes/all-notes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-notes',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-notes.component.html',
+  styleUrl: './all-notes.component.scss'
+})
+export class AllNotesComponent {
+
+}
---
+++ src/components/notes-content/notes-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './notes-content.component.html',
+  styleUrl: './notes-content.component.scss'
+})
+export class NotesContentComponent {
+
+}
