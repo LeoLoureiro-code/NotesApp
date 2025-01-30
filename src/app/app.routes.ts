e4669d8af968c9603149fd10af9b52660bd5fdf1
Leo Loureiro <leolou.90@gmail.com>
2025-01-29 19:05:23 -0700

    add route to notes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from '../components/register/register.component';
 import { LoginComponent } from '../components/login/login.component';
 import { ForgotPasswordComponent } from '../components/forgot-password/forgot-password.component';
+import { NotesComponent } from '../components/notes/notes.component';
 
 export const routes: Routes = [
 {path: '', redirectTo: 'login', pathMatch: 'full'},    
 {path: 'login', component: LoginComponent},
 {path: 'register', component: RegisterComponent},
 {path: 'forgot-password', component: ForgotPasswordComponent},
+{path: 'notes', component: NotesComponent}
 ];
