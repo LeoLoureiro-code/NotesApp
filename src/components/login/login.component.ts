baf13956e16c16f722cab3faffb1b97bb994a83a
Leo Loureiro <leolou.90@gmail.com>
2025-02-09 19:11:33 -0700

    configure form to acces server using http client

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgForm, FormsModule } from '@angular/forms';
+import { AuthenticatedResponse } from '../../interfaces/AuthenticatedResponse';
+import { LoginModel } from '../../interfaces/loginModel';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+ 
+  invalidLogin: boolean;
+  credentials: LoginModel = {username:'', password:''};
+  constructor(private router: Router, private http: HttpClient) { }
 
+  ngOnInit(): void {
+    
+  }
+
+  login = ( form: NgForm) => {
+    if (form.valid) {
+      this.http.post<AuthenticatedResponse>("https://localhost:7261/api/auth/login", this.credentials, {
+        headers: new HttpHeaders({ "Content-Type": "application/json"})
+      })
+      .subscribe({
+        next: (response: AuthenticatedResponse) => {
+          const token = response.token;
+          const refreshToken = response.refreshToken;
+          localStorage.setItem("jwt", token); 
+          localStorage.setItem("refreshToken", refreshToken);
+          this.invalidLogin = false; 
+          this.router.navigate(["notes"]);
+        },
+        error: (err: HttpErrorResponse) => this.invalidLogin = true
+      })
+    }
+  }
+ 
 }
