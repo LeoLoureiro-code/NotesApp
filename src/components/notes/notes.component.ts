656f2eaa15267b30c378c3a39a42457fea973854
Leo Loureiro <leolou.90@gmail.com>
2025-02-17 19:43:02 -0700

    add text editor to notes component

--- src/components/notes/notes.component.ts
+++ src/components/notes/notes.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../navigation/navigation.component";
+import { NotesContentComponent } from "../notes-content/notes-content.component";
+import { HeaderComponent } from "../header/header.component";
+import { TextEditorComponent } from "../text-editor/text-editor.component";
 
 @Component({
   selector: 'notes',
   standalone: true,
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, NotesContentComponent, HeaderComponent, TextEditorComponent],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss'
 })
