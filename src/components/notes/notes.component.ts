1c9db697af8959de001107c0d8542090cc76fd1d
Leo Loureiro <leolou.90@gmail.com>
2025-02-20 19:12:56 -0700

    add all notes component to the notes component

--- src/components/notes/notes.component.ts
+++ src/components/notes/notes.component.ts
@@ -3,11 +3,12 @@ import { NavigationComponent } from "../navigation/navigation.component";
 import { NotesContentComponent } from "../notes-content/notes-content.component";
 import { HeaderComponent } from "../header/header.component";
 import { TextEditorComponent } from "../text-editor/text-editor.component";
+import { AllNotesComponent } from "../all-notes/all-notes.component";
 
 @Component({
   selector: 'notes',
   standalone: true,
-  imports: [NavigationComponent, NotesContentComponent, HeaderComponent, TextEditorComponent],
+  imports: [NavigationComponent, NotesContentComponent, HeaderComponent, TextEditorComponent, AllNotesComponent],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss'
 })
