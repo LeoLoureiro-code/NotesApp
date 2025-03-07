6cff81adfecbd24d21d1caff9ca6a4a047a3617f
Leo Loureiro <leolou.90@gmail.com>
2025-03-06 18:43:49 -0700

    add mobile navigation to notes component

--- src/components/notes/notes.component.ts
+++ src/components/notes/notes.component.ts
@@ -4,11 +4,12 @@ import { NotesContentComponent } from "../notes-content/notes-content.component"
 import { HeaderComponent } from "../header/header.component";
 import { TextEditorComponent } from "../text-editor/text-editor.component";
 import { AllNotesComponent } from "../all-notes/all-notes.component";
+import { MobileNavigationComponent } from "../mobile-navigation/mobile-navigation.component";
 
 @Component({
   selector: 'notes',
   standalone: true,
-  imports: [NavigationComponent, NotesContentComponent, HeaderComponent, TextEditorComponent, AllNotesComponent],
+  imports: [NavigationComponent, NotesContentComponent, HeaderComponent, TextEditorComponent, AllNotesComponent, MobileNavigationComponent],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss'
 })
