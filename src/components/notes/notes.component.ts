1ee5f6fe47501055c377d9d3a2b278296f672abb
Leo Loureiro <leolou.90@gmail.com>
2025-03-09 18:40:58 -0700

    add modal to notes component

--- src/components/notes/notes.component.ts
+++ src/components/notes/notes.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NavigationComponent } from "../navigation/navigation.component";
 import { NotesContentComponent } from "../notes-content/notes-content.component";
 import { HeaderComponent } from "../header/header.component";
 import { TextEditorComponent } from "../text-editor/text-editor.component";
 import { AllNotesComponent } from "../all-notes/all-notes.component";
 import { MobileNavigationComponent } from "../mobile-navigation/mobile-navigation.component";
+import { ModalComponent } from "../modal/modal.component";
 
 @Component({
   selector: 'notes',
   standalone: true,
-  imports: [NavigationComponent, NotesContentComponent, HeaderComponent, TextEditorComponent, AllNotesComponent, MobileNavigationComponent],
+  imports: [NavigationComponent, NotesContentComponent, HeaderComponent, TextEditorComponent, AllNotesComponent, MobileNavigationComponent, ModalComponent, CommonModule],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss'
 })
 export class NotesComponent {
+  isVisible = true; 
 
+  openModal() {
+    this.isVisible = true; 
+  }
+
+  closeModal() {
+    this.isVisible = false; 
+  }
+
+  handleConfirmAction() {
+    console.log('Action confirmed!');
+    this.isVisible = false; 
+  }
 }
