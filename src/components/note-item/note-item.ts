d7e3aa825a9c29d0626c43155d9e5018cf085da2
Leo <leolou.90@gmail.com>
2025-10-20 18:38:40 -0700

    create note item component

---
+++ src/components/note-item/note-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-item',
+  imports: [],
+  templateUrl: './note-item.html',
+  styleUrl: './note-item.scss'
+})
+export class NoteItem {
+
+}
