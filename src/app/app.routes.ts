4511bca4266c7d273ac12a910916e74766024d54
Leo <leolou.90@gmail.com>
2025-11-04 16:06:24 -0700

    change routes to add semi-routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from '../pages/home/home';
-import { LoginPage } from '../pages/login-page/login-page';
+import { AUTH_ROUTES } from '../pages/auth-page/auth-page.routes';
 
 export const routes: Routes = [
-     {path: 'home', component: Home},
-     {path: '', redirectTo: '/login', pathMatch: 'full'},
-     {path: 'login', component: LoginPage},
+  { path: 'home', component: Home },
+  ...AUTH_ROUTES,
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
 ];
