9330636240612a9a4064bc336737103d30ad654d
Leo <leolou.90@gmail.com>
2025-11-04 16:12:58 -0700

    add routerlink to html and ts

--- src/pages/auth-page/signup/signup.ts
+++ src/pages/auth-page/signup/signup.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'signup',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './signup.html',
   styleUrl: './signup.scss'
 })
