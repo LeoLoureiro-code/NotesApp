82f16e7e633fe942649dfe989a1a8decd30aaa80
Leo <leolou.90@gmail.com>
2025-11-03 19:07:56 -0700

    move components to auth-page component

---
+++ src/pages/auth-page/signup/signup.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'signup',
+  imports: [],
+  templateUrl: './signup.html',
+  styleUrl: './signup.scss'
+})
+export class Signup {
+
+}
