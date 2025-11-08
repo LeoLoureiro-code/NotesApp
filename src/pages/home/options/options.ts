cf02549b7ec6555127970399af6fb8ceabe7e79f
Leo <leolou.90@gmail.com>
2025-11-07 19:40:35 -0700

    add options component

---
+++ src/pages/home/options/options.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-options',
+  imports: [],
+  templateUrl: './options.html',
+  styleUrl: './options.scss'
+})
+export class Options {
+
+}
