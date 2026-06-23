cc63af65127713aabb43b4bcb14464bcb8a73045
Leo <leolou.90@gmail.com>
2026-06-22 18:31:05 -0700

    add routes for the page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NotesLoginPage } from '../pages/notes-login-page/notes-login-page';
+import { NotesRegisterPage } from '../pages/notes-register-page/notes-register-page';
+import { NotesMainPage } from '../pages/notes-main-page/notes-main-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: NotesLoginPage},
+    {path: 'register', component: NotesRegisterPage},
+    {path: ':id', component: NotesMainPage},
+    {path: '', redirectTo: 'login',pathMatch: 'full'},
+];
